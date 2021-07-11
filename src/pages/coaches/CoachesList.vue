a43642b2035ec2d73cc5bf5a22d21547b4a3fa0d
melonsoda20 <benedictusteddy200596@gmail.com>
2021-07-11 16:48:17 +0700

    Create a card UI component to wrap the content of CoachItem inside CoachesList

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -4,25 +4,27 @@
       FILTER
     </section>
     <section>
-      <div class="controls">
-        <button>Refresh</button>
-        <router-link to="/register">Register as Coach</router-link>
-      </div>
-      <ul v-if="hasCoaches">
-        <coach-item
-          v-for="coach in filteredCoaches"
-          :key="coach.id"
-          :propsID="coach.id"
-          :propsFirstName="coach.firstName"
-          :propsLastName="coach.lastName"
-          :propsRate="coach.hourlyRate"
-          :propsAreas="coach.areas"
-        >
-        </coach-item>
-      </ul>
-      <h3 v-else>
-        No coaches found.
-      </h3>
+      <base-card>
+        <div class="controls">
+          <button>Refresh</button>
+          <router-link to="/register">Register as Coach</router-link>
+        </div>
+        <ul v-if="hasCoaches">
+          <coach-item
+            v-for="coach in filteredCoaches"
+            :key="coach.id"
+            :propsID="coach.id"
+            :propsFirstName="coach.firstName"
+            :propsLastName="coach.lastName"
+            :propsRate="coach.hourlyRate"
+            :propsAreas="coach.areas"
+          >
+          </coach-item>
+        </ul>
+        <h3 v-else>
+          No coaches found.
+        </h3>
+      </base-card>
     </section>
   </div>
 </template>
