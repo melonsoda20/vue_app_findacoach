a47bb11630aad33056ecd3aef226e2728d06e4a5
melonsoda20 <benedictusteddy200596@gmail.com>
2021-07-11 15:28:14 +0700

    Setup initial code for CoachesList

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <section>
+            FILTER
+        </section>
+        <section>
+            LIST OF COACHES
+        </section>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
