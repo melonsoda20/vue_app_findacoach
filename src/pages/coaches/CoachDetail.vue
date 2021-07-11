28f114b13cc58adbc052798c8f6d6de7d2e8def5
melonsoda20 <benedictusteddy200596@gmail.com>
2021-07-11 18:11:31 +0700

    Update the Coach Detail Component to retrieve data from its parent

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,7 +1,59 @@
 <template>
-    <div>
-        DETAILS FOR COACH
-        <router-view></router-view>
-        <router-link to="/coaches/c1/contact">Contact</router-link>
-    </div>
+    <section>
+        <base-card>
+            <h2>{{ computedFullName }}</h2>
+            <h3>${{ computedRate }}/hour</h3>
+        </base-card>
+    </section>
+    <section>
+        <base-card>
+            <header>
+                <h2>Interested? Reach out now!</h2>
+                <base-button propsLink :propsTo="computedContactLink">Contact</base-button>
+            </header>
+            <router-view></router-view>
+        </base-card>
+    </section>
+    <section>
+        <base-card>
+            <base-badge v-for="area in computedAreas" :key="area" 
+            :propsType="area"
+            :propsTitle="area">
+            </base-badge>
+            <p>{{ computedDescription }}</p>
+        </base-card>
+    </section>
 </template>
+
+<script>
+export default {
+    props: ['id'],
+    data(){
+        return{
+            selectedCoach: null
+        };
+    },
+    computed: {
+        computedFullName(){
+            return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
+        },
+        computedAreas(){
+            return this.selectedCoach.areas;
+        },
+        computedRate(){
+            return this.selectedCoach.hourlyRate;
+        },
+        computedDescription(){
+            return this.selectedCoach.description;
+        },
+        computedContactLink(){
+            return this.$route.path + '/' + this.id + '/contact';
+        }
+    },
+    created(){
+        this.selectedCoach = this.$store.getters['coaches/coaches'].find(
+            coach => coach.id === this.id
+        );
+    }
+}
+</script>
