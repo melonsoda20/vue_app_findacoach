3a5baedf8a6acbba29ec96daaae863c01d305fae
melonsoda20 <benedictusteddy200596@gmail.com>
2021-07-11 16:40:33 +0700

    Update coaches list component to use CoachItem component to render the data

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,38 +1,59 @@
 <template>
-    <div>
-        <section>
-            FILTER
-        </section>
-        <section>
-            <div class="controls">
-                <button>Refresh</button>
-                <router-link to="/register">Register as Coach</router-link>
-            </div>
-            <ul v-if="hasCoaches">
-                <li v-for="(coach) in filteredCoaches" :key="coach.id">
-                    {{ coach.firstName }}
-                </li>
-            </ul>
-            <h3 v-else>
-                No coaches found.
-            </h3>
-        </section>
-    </div>
+  <div>
+    <section>
+      FILTER
+    </section>
+    <section>
+      <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register">Register as Coach</router-link>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :propsID="coach.id"
+          :propsFirstName="coach.firstName"
+          :propsLastName="coach.lastName"
+          :propsRate="coach.hourlyRate"
+          :propsAreas="coach.areas"
+        >
+        </coach-item>
+      </ul>
+      <h3 v-else>
+        No coaches found.
+      </h3>
+    </section>
+  </div>
 </template>
 
 <script>
-export default{
-    computed:{
-        filteredCoaches(){
-            return this.$store.getters['coaches/coaches'];
-        },
-        hasCoaches(){
-            return this.$store.getters['coaches/hasCoaches']
-        }
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
+export default {
+  components: {
+    CoachItem
+  },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
     }
-}
+  }
+};
 </script>
 
 <style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
