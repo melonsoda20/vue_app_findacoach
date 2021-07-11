c2d94fdd0a60968014bcc9aefbcff4441b47136e
melonsoda20 <benedictusteddy200596@gmail.com>
2021-07-11 14:55:02 +0700

    Add initial components to be used in this app

--- src/router.js
+++ src/router.js
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoacheslList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -9,27 +16,27 @@ const router = createRouter({
         },
         {
             path: '/coaches', 
-            component: null
+            component: CoacheslList
         },
         {
             path: '/coaches/:id', 
-            component: null,
+            component: CoachDetail,
             children: {
                 path: 'contact',
-                component: null
+                component: ContactCoach
             }
         },
         {
             path: '/register', 
-            component: null
+            component: CoachRegistration
         },
         {
             path: '/requests',
-            component: null
+            component: RequestsReceived
         },
         {
             path: '/:notFound(.*)',
-            component: null
+            component: NotFound
         }
     ]
 });
