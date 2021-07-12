6de38d933ac5b072b169d1192a51248d3000379c
melonsoda20 <benedictusteddy200596@gmail.com>
2021-07-12 14:26:28 +0700

    Add feature to load data from cache based on timestamp

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -9,7 +9,7 @@
     <section>
       <base-card>
         <div class="controls">
-          <base-button propsMode="outline" @click="loadCoaches">Refresh</base-button>
+          <base-button propsMode="outline" @click="loadCoaches(true)">Refresh</base-button>
           <base-button v-if="!isCoach && !isLoading" propsLink propsTo="/register">Register as Coach</base-button>
         </div>
         <div v-if="isLoading">
@@ -85,10 +85,10 @@ export default {
     setFilters(updatedFilters){
       this.activeFilters = updatedFilters;
     },
-    async loadCoaches(){
+    async loadCoaches(refresh = false){
       this.isLoading = true;
       try{
-        await this.$store.dispatch('coaches/loadCoaches');
+        await this.$store.dispatch('coaches/loadCoaches', { forceResfresh: refresh });
       }catch(error){
         this.error = error.message || 'Something went wrong!';
       }
