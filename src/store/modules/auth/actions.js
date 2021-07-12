a1e9f85c8b35366a6f0b27ca61ea93c14349ac15
melonsoda20 <benedictusteddy200596@gmail.com>
2021-07-12 18:54:32 +0700

    Store userId and token in local storage

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,30 +1,24 @@
 export default{
     async login(context, payload){
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB-PiL8ynBluBIKD9Tfjk4Mu28HUR1Q3fQ', {
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true
-            })
-        });
-
-        const responseData = await response.json();
-
-        if(!response.ok){
-            console.log(responseData);
-            const error = new Error(responseData.message || 'Failed to authenticate.');
-            throw error;
-        }
-
-        context.commit('setUser', {
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn
+        context.dispatch('auth', {
+            ...payload,
+            mode: 'login'
         });
     },
     async signup(context, payload){
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB-PiL8ynBluBIKD9Tfjk4Mu28HUR1Q3fQ', {
+        context.dispatch('auth', {
+            ...payload,
+            mode: 'signup'
+        });
+    },
+    async auth(context, payload){
+        const mode = payload.mode;
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB-PiL8ynBluBIKD9Tfjk4Mu28HUR1Q3fQ';
+
+        if(mode === 'signup'){
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB-PiL8ynBluBIKD9Tfjk4Mu28HUR1Q3fQ';
+        }
+        const response = await fetch(url, {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
@@ -41,12 +35,28 @@ export default{
             throw error;
         }
 
-        console.log(responseData);
+        localStorage.setItem('token', responseData.idToken);
+        localStorage.setItem('userId', responseData.localId);
+
         context.commit('setUser', {
             token: responseData.idToken,
             userId: responseData.localId,
             tokenExpiration: responseData.expiresIn
         });
+
+
+    },
+    tryLogin(context){
+        const token =localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
+
+        if(token && userId){
+            context.commit('setUser', {
+                token: token,
+                userId: userId,
+                tokenExpiration: null
+            });
+        }
     },
     logout(context){
         context.commit('setUser', {
