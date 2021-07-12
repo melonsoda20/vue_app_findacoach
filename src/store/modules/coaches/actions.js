d68ece02586a68a261cfc59806e27c73c145970f
melonsoda20 <benedictusteddy200596@gmail.com>
2021-07-12 17:38:51 +0700

    Fix the issue with getting token from vuex

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -9,10 +9,13 @@ export default{
             areas: data.areas
         };
 
-        const response = await fetch(`https://vue-http-demo-54ccf-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json`, {
+        const token = context.rootGetters.token;
+
+        const response = await fetch(`https://vue-http-demo-54ccf-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json?auth=` + token, {
             method: 'PUT',
             body: JSON.stringify(coachData)
         });
+
 
         // const responseData = await response.json();
 
